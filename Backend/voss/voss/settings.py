316e8be087c67ce3196164b47ab95c9fdfc9e455
pjj11005 <pjj11005@gmail.com>
2024-08-16 16:23:43 +0900

    doc : delete email password

--- Backend/voss/voss/settings.py
+++ Backend/voss/voss/settings.py
@@ -62,7 +62,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'masters1004@gmail.com'
-EMAIL_HOST_PASSWORD = 'fnyvloxrljftbwby'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
